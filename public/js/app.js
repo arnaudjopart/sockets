b43b3449bea02bcda2df12febb75f36c82ec4cd3
Arnaud Jopart <arnaud.jopart@gmail.com>
2016-10-17 14:46:18 +0200

    chat is working

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,20 @@
-var socket = io();
-
+var socket = io("http://localhost:3000");
+console.log("Test");
 socket.on('connect',function(){
   console.log('Connected to socket.io server!');
 });
 
+var $messages = jQuery('.messages');
+var $form = jQuery('#message-form');
+
+$form.on('submit',function(event){
+	event.preventDefault();
+	var $message = $form.find('input[name=message]');
+	socket.emit('message',{text:$message.val()});
+	$message.val("");
+});
+
 socket.on('message',function(message){
   console.log("new message: "+message.text);
+  $messages.append("<p>"+message.text+"<p>");
 });
